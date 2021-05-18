03d64e6ff612a6b5427526a81f95e50d2c08357e
Alex <psionickmobile@gmail.com>
2021-05-18 11:38:42 +0300

    upd indexes

--- 3_indexes.sql
+++ 3_indexes.sql
@@ -1,9 +1,6 @@
 -- Index for actors
 CREATE INDEX actors_first_name_last_name_idx ON actors(first_name, last_name);
 
--- Index for genres
-CREATE INDEX genres_genre_idx ON genres(genre);
-
 -- Indexes for movies
 CREATE INDEX movies_title_idx ON movies(title);
 CREATE INDEX movies_title_short_desription_idx ON movies(title, short_desription);
@@ -11,5 +8,3 @@ CREATE INDEX movies_title_short_desription_idx ON movies(title, short_desription
 -- Index for profiles
 CREATE INDEX profiles_first_name_last_name_idx ON profiles(first_name, last_name);
 
--- Index for users
-CREATE INDEX users_email_uq ON users(email);
